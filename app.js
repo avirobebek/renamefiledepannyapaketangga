47db52af6f3923a3250316499ca5a0baf44ab8c3
Aufar Sukmajaya <aufarsukmajaya@gmail.com>
2018-10-06 07:29:22 +0700

    better regex

--- app.js
+++ app.js
@@ -9,20 +9,25 @@ function getVideo(items, count){
   var file = path + '/' + items[count];
   ffmetadata.read(file, function(err, data) {
     if (err){console.error("Error reading metadata", err)};
-    if(typeof data.comment != 'undefined'){
-      videoId = data.comment.split('=')[1];
-      fetchVideoInfo(videoId).then(function (videoInfo) {
-        splittedItems = items[count].split(".");
-        delete videoInfo["regionsAllowed"];
-        delete videoInfo["description"];
-        fs.rename(file, path + '/' + videoInfo.datePublished.replace(/-/g, '')+" "+videoInfo.title+"."+splittedItems[splittedItems.length-1], function(err) {if ( err ) console.log('ERROR: ' + err);});
-        console.log(videoInfo);
-        console.log(videoId+" == "+videoInfo.datePublished);
-        setTimeout(function(){ getVideo(items, (count+1)), 500 });
-      });
+    if(typeof data != 'undefined'){
+      if(typeof data.comment != 'undefined'){
+        videoId = data.comment.split('=')[1];
+        fetchVideoInfo(videoId).then(function (videoInfo) {
+          splittedItems = items[count].split(".");
+          delete videoInfo["regionsAllowed"];
+          delete videoInfo["description"];
+          fs.rename(file, path + '/' + videoInfo.datePublished.replace(/-/g, '')+" "+videoInfo.title.replace(/[^\w.-\s]+/g, '')+"."+splittedItems[splittedItems.length-1], function(err) {if ( err ) console.log('ERROR: ' + err);});
+          console.log("\n\n"+videoInfo.title);
+          console.log(videoId+" == "+videoInfo.datePublished);
+          setTimeout(function(){ getVideo(items, (count+1)), 500 });
+        });
+      }else{
+        //console.log("\n"+items[count]+ " have no comment metadata"+"\n");
+        setTimeout(function(){ getVideo(items, (count+1)), 1 });
+      }
     }else{
-      console.log("\n"+items[count]+ " have no comment metadata"+"\n");
-      setTimeout(function(){ getVideo(items, (count+1)), 500 });
+      //console.log("\n"+items[count]+ " have no comment metadata"+"\n");
+      setTimeout(function(){ getVideo(items, (count+1)), 1 });
     }
   });
 }
